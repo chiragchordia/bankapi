f865ed3aec8dda78af8041df8f49ba9cb6441c60
jain chirag <cjain9497@gmail.com>
2019-02-16 23:45:51 +0530

    added pg module

--- app.js
+++ app.js
@@ -1,65 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mysql = require('mysql'); // MS Sql Server client
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const db = require('./queries/queries')
+const port = 7000
 
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var bankRouter = require('./routes/bank');
-
-var app = express();
-const bodyparser = require('body-parser');
-app.use(bodyparser.json());
-// Connection string parameters.
-global.connection = mysql.createConnection({
-  host: 'ec2-23-21-165-188.compute-1.amazonaws.com',
-  user: 'mbfqfcdptylngh',
-  password: 'cb8728d1f2c34c8cc1b0891f6b99757240c3ce5996b7821e6d8539498a2845fe',
-  database: 'd2uv20f2cqaj1u'
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
 })
 
-connection.connect(function (error) {
-  if (!!error) {
-    console.log(error);
-    console.log("error");
-  }
-  else {
-    console.log('Connected Succesfully.!');
-  }
+app.get('/bankbranch/:ifsc', db.getBanksByIfsc)
+app.get('/bankdetails/:bankname/:city', db.getBanksDetails)
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
 })
-
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/bank', bankRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
--- queries/queries.js
+++ queries/queries.js
@@ -1,4 +1,38 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+	user: 'mbfqfcdptylngh',
+	host: 'ec2-23-21-165-188.compute-1.amazonaws.com',
+	database: 'd2uv20f2cqaj1u',
+	password: 'cb8728d1f2c34c8cc1b0891f6b99757240c3ce5996b7821e6d8539498a2845fe',
+	port: 5432,
+})
+pool.on('error', (err, client) => {
+	console.error('Unexpected error on idle client', err)
+	process.exit(-1)
+})
+
+const getBanksByIfsc = (request, response) => {
+	const ifsc = parseInt(request.params.id)
+	pool.query('select * from bank_details where bank_ifsc=$1', [ifsc], (error, results) => {
+		if (error) {
+			throw error
+		}
+		response.status(200).json(results.rows)
+	})
+}
+
+const getBanksDetails = (request, response) => {
+	const bankName = req.params.bankname;
+	const bankCity = req.params.city;
+	pool.query('select * from bank_details where bank_name=$1 and bank_city=$2', [bankName], [bankCity], (error, results) => {
+		if (error) {
+			throw error
+		}
+		response.status(200).json(results.rows)
+	})
+}
+
 module.exports = {
-	query_fetch_branch: "select * from bank_details where bank_ifsc=?",
-	query_fetch_details: "select * from bank_details where bank_name=? and bank_city=?"
+	getBanksByIfsc,
+	getBanksDetails,
 }
