24c1d0ceb60ebf74452a5931513df99c92f4b837
jain chirag <cjain9497@gmail.com>
2019-02-16 23:58:19 +0530

    ad

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express')
+const createError = require('http-errors');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries/queries')
 const port = 7000
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json())
 app.use(
@@ -17,6 +30,17 @@ app.get('/', (request, response) => {
 
 app.get('/bankbranch/:ifsc', db.getBanksByIfsc)
 app.get('/bankdetails/:bankname/:city', db.getBanksDetails)
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
